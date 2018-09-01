ac6a219baba2a1187b6adbdcd9bc63a469e7d480
Eviber <youv.didj@gmail.com>
2018-09-01 12:04:43 +0200

    add sprite and rough sprite draw

--- main.lua
+++ main.lua
@@ -9,15 +9,31 @@ local game = gs.game
 local lg = love.graphics
 local isDown = love.keyboard.isDown
 
+function loadSprite()
+	image = lg.newImage("sprite.png")
+	local width = image:getWidth()
+	local height = image:getHeight()
+	local frameW = 24
+	local frameH = 32
+
+	frames = {}
+	for i=0,4 do
+		table.insert(frames, lg.newQuad(1 + i*frameW + i, 1, frameW, frameH, width, height))
+	end
+	currentFrame = 1
+	dir = 1
+end
+
 function love.load()
 	W, H = 800, 600
 	love.window.setMode(W, H)
     Gamestate.registerEvents()
     Gamestate.switch(gs.start)
+	loadSprite()
 end
 
 function game:enter()
-	player = {x = 0, y = 0, w = 10, h = 10, speed = 300}
+	player = {x = 0, y = 0, w = 10, h = 10, speed = 100}
 	player.x = W/2 - player.w/2
 	player.y = H/2 - player.h/2
 end
@@ -30,18 +46,29 @@ function game:update(dt)
 	end
 	if isDown("up") then
 		player.y = player.y - player.speed * dt
+		currentFrame = 5
 	end
 	if isDown("down") then
 		player.y = player.y + player.speed * dt
+		currentFrame = 1
 	end
 	if isDown("left") then
 		player.x = player.x - player.speed * dt
+		if isDown("up") then		currentFrame = 4
+		elseif isDown("down") then	currentFrame = 2
+		else 						currentFrame = 3 end
+		dir = -1
 	end
 	if isDown("right") then
 		player.x = player.x + player.speed * dt
+		currentFrame = 3
+		if isDown("up") then		currentFrame = 4
+		elseif isDown("down") then	currentFrame = 2
+		else 						currentFrame = 3 end
+		dir = 1
 	end
 end
 
 function game:draw()
-    lg.rectangle("fill", player.x, player.y, player.w, player.h)
+    lg.draw(image, frames[currentFrame], player.x, player.y, 0, dir, 1, 12, 16)
 end
