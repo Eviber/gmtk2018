4150b5c2cacb1d6200bb9b7eb0277016b342937e
Eclahn <lgdtheo@gmail.com>
2018-09-01 18:43:44 +0200

    entity draw

--- controls.lua
+++ controls.lua
@@ -2,7 +2,6 @@
 function gs.game:mousepressed(x, y, button, istouch, presses)
 	local minProx = nil
 	local closest = nil
-	print(player)
 	for i, entity in pairs(EntitiesList) do
 		local proximity = math.sqrt((entity.x + 8 - x)^2 + (entity.y + 8 - y)^2)
 		if  proximity < 20 and (minProx == nil or minProx > proximity) and entity ~= player then
@@ -10,7 +9,6 @@ function gs.game:mousepressed(x, y, button, istouch, presses)
 			closest = entity
 		end
 	end
-	print(closest)
 	if closest ~= nil then
 		player:swap(closest)
 	end
