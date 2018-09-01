1c07ddc63956ee6c89301444f1b18da4cf17d9e1
Eviber <youv.didj@gmail.com>
2018-09-01 06:19:08 +0200

    add placeholder start and end screens

--- main.lua
+++ main.lua
@@ -1,17 +1,33 @@
 local Gamestate = require "hump.gamestate"
 
-local game = {}
+gs = {}
+gs.game = {}
+
+require "startend"
+
+local game = gs.game
 local lg = love.graphics
 local isDown = love.keyboard.isDown
 
-player = {x = 0, y = 0, w = 10, h = 10, speed = 300}
-
 function love.load()
+	W, H = 800, 600
+	love.window.setMode(W, H)
     Gamestate.registerEvents()
-    Gamestate.switch(game)
+    Gamestate.switch(gs.start)
+end
+
+function game:enter()
+	player = {x = 0, y = 0, w = 10, h = 10, speed = 300}
+	player.x = W/2 - player.w/2
+	player.y = H/2 - player.h/2
 end
 
 function game:update(dt)
+	local p = player
+	local p = player
+	if p.x < 0 or p.x + p.w > W or p.y < 0 or p.y + p.h > H then
+		Gamestate.switch(gs.over)
+	end
 	if isDown("up") then
 		player.y = player.y - player.speed * dt
 	end
