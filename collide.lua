9b1a83a3ba1ad5f9c112545b162f3f1e2e483802
Eviber <youv.didj@gmail.com>
2018-09-02 00:28:04 +0200

    no more collision between bullets

--- collide.lua
+++ collide.lua
@@ -1,5 +1,7 @@
 function filter(item, other)
-	if item.id == 69 or other.id == 69 then
+	if item.id == 69 and other.id == 69 then
+		return false
+	elseif item.id == 69 or other.id == 69 then
 		return "cross"
 	end
 	return "slide"
