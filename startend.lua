83e59366754cf661f923d90846ee78d566e1322a
Eclahn <lgdtheo@gmail.com>
2018-09-02 22:04:02 +0200

    How To Play now functional

--- startend.lua
+++ startend.lua
@@ -8,6 +8,7 @@ gs.howToPlay = {}
 local items, len
 local timer = 0
 local clickedPlay = false
+local clickedHtp = false
 
 local start = gs.start
 local over = gs.over
@@ -35,7 +36,9 @@ function gs.start:mousepressed(x, y, button, istouch, presses)
 			--Gamestate.switch(gs.game)
 			clickedPlay = true
 		elseif items[1] == 2 then
-			Gamestate.switch(gs.howToPlay)
+			fakePlayer = peachy.new("assets/fx_swap.json", lg.newImage("assets/fx_swap.png"), "blink")
+			fakePlayer:play()
+			clickedHtp = true
 		end
 	end
 end
@@ -44,13 +47,18 @@ function start:update(dt)
 	local x, y = love.mouse.getPosition()
 	items, len = menuHighlight:queryPoint(x, y) 
 	fakePlayer:update(dt)
-	if clickedPlay == true then
+	if clickedPlay == true or clickedHtp == true then
 		timer = timer + dt
 		if timer > 0.2 then
 			timer = 0
-			clickedPlay = false
 			fakePlayer = peachy.new("assets/player.json", love.graphics.newImage("assets/player.png"), "walk_D")
-			Gamestate.switch(gs.game)
+			if clickedHtp == true then
+				clickedHtp = false
+				Gamestate.switch(gs.howToPlay)
+			else
+				clickedPlay = false
+				Gamestate.switch(gs.game)
+			end
 		end
 	end
 end
@@ -77,9 +85,9 @@ function start:draw()
 	--lg.print("Actually this is a placeholder and you need to press Enter to play lul", W/2 - 150, H/2 + 200)
 	if len ~= 0 then
 		if items[1] == 1 then
-			fakePlayer:draw(W/2 - 100, H/2)
+			fakePlayer:draw(clickedPlay == true and W/2 - 120 or W/2 - 100, clickedPlay == true and H/2 - 16 or H/2)
 		elseif items[1] == 2 then
-			fakePlayer:draw(W/2 - 100, H/2 + 60)
+			fakePlayer:draw(clickedHtp == true and W/2 - 120 or W/2 - 100, clickedHtp == true and H/2 + 44 or H/2 + 60)
 		elseif items[1] == 3 then
 			fakePlayer:draw(W/2 - 100, H/2 + 120)
 		end
@@ -98,9 +106,24 @@ function printGameOver()
 end
 ]]
 
-function over:update(dt)
+function htp:draw()
+	lg.setColor(1,1,1,1)
+	lg.setFont(subtitleFont)
+	lg.print("WASD.........................................Move", W/2 - 300, H/2 - 50)
+	lg.print("Right Click on enemy........Swap places", W/2 - 300, H/2 + 50)
+	lg.print("Press Enter to go back to menu", W/2 - 250, H/2 + 200)
+end
+
+function htp:update()
 	if isDown("return") then
 		Gamestate.switch(start)
+	end
+end
+
+
+function over:update(dt)
+	if isDown("return") then
+		love.load()
 	elseif isDown("escape") then
 		love.event.quit()
 	end
