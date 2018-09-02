2e99abf91c873c14da5a105f96532379a97e4392
Tristan Duquesne <fulguritude@macbook-air-de-tristan.home>
2018-09-02 18:40:32 +0200

    try to init entities while reading map string, some errors

--- main.lua
+++ main.lua
@@ -16,7 +16,7 @@ local isDown = love.keyboard.isDown
 local cursorImg = lg.newImage("assets/cursor.png")
 
 function love.load()
-	tilesize = 16
+	local tilesize = TILESIZE
 	W_tiles, H_tiles = 60, 40
 	W = W_tiles * tilesize
 	H = H_tiles * tilesize
@@ -24,13 +24,9 @@ function love.load()
 	Gamestate.registerEvents()
 	Gamestate.switch(gs.start)
 	coll = bump.newWorld()
-	player = Player(1, W/2, H/2, 100, 100)
-	--testEnemy = RifleShooter(1, 100, 100)
-	--yourDumb = RifleShooter(2, 200, 100)
-	--iHitYou = Brawler(3, 300, 100)
-	for i = 0, 10 do
-		Brawler(math.random(16, W - 32), math.random(16, H - 32))
-	end
+	--for i = 0, 10 do
+	--	Brawler(math.random(16, W - 32), math.random(16, H - 32))
+	--end
 	fx_blink = peachy.new("assets/fx_swap.json", lg.newImage("assets/fx_swap.png"), "blink")
 	fx_blink:onLoop(function() player.swapping = false player:swap(player.swap_target) end)
 	fx_smoke = peachy.new("assets/fx_swap.json", lg.newImage("assets/fx_swap.png"), "smoke")
@@ -66,11 +62,11 @@ function love.load()
 		"XXXXXXXXXXXXXXXXXXXXXXXXXX----XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
 		"X-----------------------X------X---------------------------X",
 		"X-----------------------X------X---------------------------X",
+		"X----------P------------X------X------------S--------------X",
 		"X-----------------------X------X---------------------------X",
 		"X-----------------------X------X---------------------------X",
 		"X-----------------------X------X---------------------------X",
-		"X-----------------------X------X---------------------------X",
-		"X----------------------------------------------------------X",
+		"X----------B-----------------------------------------------X",
 		"X----------------------------------------------------------X",
 		"X-----------------------X------X---------------------------X",
 		"X-----------------------X------X---------------------------X",
@@ -109,7 +105,7 @@ end
 
 function draw_mouse_scope()
 	local x, y = love.mouse.getX(), love.mouse.getY()
-	lg.draw(cursorImg, x - 8, y - 8) --, 0, 1, 16, 16)
+	lg.draw(cursorImg, x - 8, y - 8)
 end
 
 function game:draw()
