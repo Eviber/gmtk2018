25de1442317c5a63e3a4280b7835287f2f4836f5
Eviber <youv.didj@gmail.com>
2018-09-02 08:49:26 +0200

    add Brawler's attack and indent

--- brawler.lua
+++ brawler.lua
@@ -2,36 +2,74 @@ require "collide"
 
 Brawler = Class{
 	__includes = Entity,
-	init = function(self, id, x, y)
-		Entity.init(self, id, x, y, 50, 50)
+	init = function(self, x, y)
+		Entity.init(self, "Brawler", x, y, 50, 50)
 		self.damage = 20
 		self.cooldown = 3
-	end,
-	attack = function(self)
-		Bullet(69, self.x, self.y, player.x - self.x, player.y - self.y)
+		self.attacking = false
+		self.target = {}
 	end,
 	draw = function(self)
 		lg.setColor(1,0,1,1)
 		lg.rectangle("fill", self.x, self.y, 16, 16)
+		if self.attacking then
+			lg.setColor(self.tBox.r, self.tBox.g, self.tBox.b, self.tBox.a)
+			lg.	rectangle("fill", self.tBox.x + self.x, self.tBox.y + self.y, 16, 16)
+		end
 		--lg.setColor(0,1,0,1)
 		--lg.rectangle("line", coll:getRect(self))
-	end,
-	update = function(self, dt)
-		if self.health <= 0 then
-			table.remove(EntitiesList, self.idx)
-			coll:remove(self)
-			return
-		end
-		self.cooldown = self.cooldown - dt
-		playerDist = math.sqrt((self.x - player.x)^2 + (self.y - player.y)^2)
-		if playerDist < 30 and self.cooldown <= 0 then
-			self:attack()
-			self.cooldown = 2
-		elseif playerDist > 30 then
-			self.dx = player.x - self.x
-			self.dy = player.y - self.y
-			self:normalize()
-			self.x, self.y = coll:move(self, self.x + self.dx * dt, self.y + self.dy * dt, filter)
-		end
 	end
 }
+
+function Brawler:update(dt)
+	if self.health <= 0 then
+		table.remove(EntitiesList, self.idx)
+		coll:remove(self)
+		return
+	end
+	self.cooldown = self.cooldown - dt
+	playerDist = math.sqrt((self.x - player.x)^2 + (self.y - player.y)^2)
+	if playerDist < 30 and not self.attacking and self.cooldown <= 0 then
+		self.target.x = player.x
+		self.target.y = player.y
+		self.attacking = true
+		self.cooldown = 2
+		self:attack(dt)
+	elseif self.attacking then
+		self:attack(dt)
+	elseif playerDist > 30 then
+		self.dx = player.x - self.x
+		self.dy = player.y - self.y
+		self:normalize()
+		self.x, self.y = coll:move(self, self.x + self.dx * dt, self.y + self.dy * dt, filter)
+	end
+end
+
+function Brawler:attack(dt)
+	if not self.tBox then
+		self.tBox = {id="fist", lock=false, x=self.target.x-self.x, y=self.target.y-self.y, r=1, g=1, b=0, a=0.5}
+	end
+	if self.cooldown <= 0 then
+		self.attacking = false
+		self.tBox = nil
+	elseif self.cooldown <= 0.5 then
+		self.tBox.g = 0
+		self.tBox.lock = true
+		local items = coll:queryRect(self.tBox.x + self.x, self.tBox.y + self.y, 16, 16,
+		function(item)
+			if not item.health or item == self then
+				return false
+			end
+			return "cross"
+		end)
+		for _, item in pairs(items) do
+			item.health = item.health - self.damage
+			local dx = item.x - self.x
+			local dy = item.y - self.y
+			local norm = math.sqrt(dx^2 + dy^2)
+			local x = item.x + dx / norm * 100
+			local y = item.y + dy / norm * 100
+			item.x, item.y = coll:move(item, x, y, filter)
+		end
+	end
+end
